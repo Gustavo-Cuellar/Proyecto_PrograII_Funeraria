33af497525bd0d4a2435d2372c588851d597fb32
Alejandro <Alejandro@Alejandro-PC>
2018-06-03 11:04:08 -0600

    Prueba Trabajo en linea

---
+++ src/proyecto_prograii_funeraria/Proyecto_PrograII_Funeraria.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_prograii_funeraria;
+
+/**
+ *
+ * @author Alejandro
+ */
+public class Proyecto_PrograII_Funeraria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hola");
+    }
+    
+}
