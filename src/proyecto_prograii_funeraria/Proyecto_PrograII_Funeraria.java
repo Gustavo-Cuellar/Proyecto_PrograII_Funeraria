d3f1bf5d65532cdc16931b16a39540b2155fae83
Alejandro-PC <>
2018-06-03 11:18:08 -0600

    prueba 2

--- src/proyecto_prograii_funeraria/Proyecto_PrograII_Funeraria.java
+++ src/proyecto_prograii_funeraria/Proyecto_PrograII_Funeraria.java
@@ -17,6 +17,7 @@ public class Proyecto_PrograII_Funeraria {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola");
+           System.out.println("que hace");
     }
     
 }
